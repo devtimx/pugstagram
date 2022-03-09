9441f1eaca3c0edc771c338511c96b21b038c64c
manuelmannuel <manuelmannuel@gmail.com>
2022-03-09 13:02:50 -0600

    End project

---
+++ README.md
@@ -0,0 +1,7 @@
+# PugStagram
+
+Ejemplo de implementación front-end con svelte consumiendo API simple
+
+@babel/core @babel/preset-env @babel/polyfill babel-loader svelte-loader
+webpack webpack-cli html-webpack-plugin
+webpack-dev-server
---
+++ src/components/Footer.svelte
@@ -0,0 +1,14 @@
+<div class="Footer">
+    <div class="Footer-container">
+        <div class="Footer-copy">2022 PugStagram</div>
+    </div>
+</div>
+
+<style>
+    .Footer {
+      font-size: 11px;
+      letter-spacing: 1px;
+      font-weight: normal;
+      color: #c7c7c7;
+    }
+  </style>
